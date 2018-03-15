ea6417a8cbd33860c4bea9ef0d886573052bdda7
bobitluo <bobitluo@gmail.com>
2018-03-15 12:06:27 +0800

    modify cfg.py

--- capt/cfg.py
+++ capt/cfg.py
@@ -1,4 +1,6 @@
 # 验证码中的字符, 就不用汉字了
+import os
+
 from os.path import join
 
 number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -7,7 +9,7 @@ ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-gen_char_set = number + ALPHABET  # 用于生成验证码的数据集
+gen_char_set = number + ALPHABET + alphabet  # 用于生成验证码的数据集
 # 有先后的顺序的
 
 # 图像大小
@@ -23,13 +25,15 @@ CHAR_SET_LEN = len(gen_char_set)
 
 print('CHAR_SET_LEN:', CHAR_SET_LEN)
 
-home_root = '/home/harmo'  # 在不同操作系统下面Home目录不一样
-workspace = join(home_root, 'work/crack/my-capt-data/capt-python-36')  # 用于工作的训练数据集
-model_path = join(home_root, 'work/crack/model')
+home_root = os.path.split(os.path.realpath(__file__))[0]
+home_root = join(home_root, '../data')
+
+workspace = join(home_root, 'workspace')  # 用于工作的训练数据集
+model_path = join(home_root, 'model')
 model_tag = 'crack_capcha.model'
 save_model = join(model_path, model_tag)
 
 print('model_path:', save_model)
 
 # 输出日志 tensorboard监控的内容
-tb_log_path = '/tmp/mnist_logs'
+tb_log_path = join(home_root, 'mnist_logs')
